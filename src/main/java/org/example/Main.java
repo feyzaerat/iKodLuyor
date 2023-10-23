06f27961d2fd3aeb81f8ed00283ef0e3d402d82c
feyzaerat <feyzakrnfl@gmail.com>
2023-10-23 14:03:07 +0300

    iKodLuyor first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "Merhaba Tobeto";
+        System.out.println(text);
+
+        Integer number = 10;
+
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        System.out.println(numbers);
+
+        //Scope => Kapsam, alan, bölge, bir kodun kapsadığı alan...
+    }
+}
